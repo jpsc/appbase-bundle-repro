74e9ca6a485590a62b7b920d50541d59cdf99d78
jpsc <joao@funda.nl>
2021-03-18 13:38:53 +0100

    inital commit

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <ReactiveBase
+      app="good-books-yj"
+      credentials="gBgUqs2tV:3456f3bf-ea9e-4ebc-9c93-08eb13e5c87c"
+    >
+    </ReactiveBase>
+  </div>
+</template>
+
+<script>
+import { ReactiveBase } from "@appbaseio/reactivesearch-vue";
+
+export default {
+  components: {
+    ReactiveBase
+  },
+  name: "app"
+};
+</script>
